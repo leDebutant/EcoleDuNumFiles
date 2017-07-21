fe18b494fde33d363f56d1c0f7cbec224235738e
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-21 16:03:27 +0200

    rajout de produit.php avec les getter et les setters

---
+++ 18_OOP/Hydratation/Produit.php
@@ -0,0 +1,96 @@
+<?php
+//Produit.php
+
+class Produit
+{
+  private $id;
+  private $nom;
+  private $description;
+  private $prix;
+  private $couleur;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * @param mixed $prix
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCouleur()
+    {
+        return $this->couleur;
+    }
+
+    /**
+     * @param mixed $couleur
+     */
+    public function setCouleur($couleur)
+    {
+        $this->couleur = $couleur;
+    }
+
+  //accesseur mutateur --> getters et les setters
+
+
+}
+ ?>
