439dd3d4993a39c083ce40aceb44f013e81e1ad7
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-24 11:41:27 +0200

    push Produit.php dans hydratation

--- 18_OOP/Hydratation/BddManager.php
+++ 18_OOP/Hydratation/BddManager.php
@@ -20,15 +20,42 @@ class BddManager
     public function getProduitById($id){
         $this->getConnexion();
 
-        $object = $this->connexion->prepare('SELECT * 
-        FROM product WHERE id=:id');
+        $object = $this->connexion->prepare('SELECT *
+        FROM produit WHERE id=:id');
         $object->execute(array(
             'id'=>$id
         ));
         $produits = $object->fetchAll(PDO::FETCH_ASSOC);
 
+        if(!empty($produits)){
+          return new Produit($produits[0]);
+        }
+        return false;
         //Ici nous sommes en train de passer un tableau!!
-        return new Produit($produits[0]);
+    }
+
+    /**
+    * getAllProduit retourne un tableau avec un object
+    * Produit dans chaque index (ligne)
+    */
+
+    public function getAllProduit(){
+      $this->getConnexion();
+
+      $object = $this->connexion->prepare('SELECT *
+      FROM produit');
+      $object->execute(array());
+      $produits = $object->fetchAll(PDO::FETCH_ASSOC);
+
+
+        if(!empty($produits)){
+            $tableauProduit=[];
+            foreach($produits as $tableauDonneesProduit){
+              $tableauProduit[]=new Produit($tableauDonneesProduit);
+            }
+            return $tableauProduit;
+        }
+        return false;
     }
 
 }
